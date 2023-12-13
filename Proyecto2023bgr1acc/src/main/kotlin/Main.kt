19987a4fc9050a72ec9a31cbe5628fe97159eece
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2023-12-13 16:28:42 -0500

    hasta instanciar

--- Proyecto2023bgr1acc/src/main/kotlin/Main.kt
+++ Proyecto2023bgr1acc/src/main/kotlin/Main.kt
@@ -39,5 +39,82 @@ fun main() {
 
     val coqueteo = if (estadoCivilWhen == "S") "Si" else "No"
 
+    calcularSueldo(10.00)
+    calcularSueldo(10.00, 15.00)
+    calcularSueldo(10.00, 12.00, 20.00)
+    //Parametros nombrados
+    calcularSueldo(sueldo = 10.00)
+    calcularSueldo(sueldo = 10.00, bonoEspecial = 15.00)
+    calcularSueldo(sueldo = 10.00, bonoEspecial = 12.00, tasa = 20.00)
+
+    calcularSueldo(10.00, bonoEspecial = 20.00) //Named Parameters
+    calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) //Parámetros nombrados
+
+    calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) //Parámetros nombrados
+
+    val sumaUno = Suma(1, 1)
+    val sumaDos = Suma(null, 1)
+    val sumaTres = Suma(1, null)
 
 }
+
+//Clases
+abstract class Numeros(
+    protected  val numeroUno: Int,
+    protected val numeroDos: Int,
+){
+    init {
+        this.numeroUno; this.numeroDos;
+        numeroUno; numeroDos;
+        println("Inicializando")
+
+    }
+}
+
+class Suma(
+    uno: Int,
+    dos: Int,
+): Numeros(uno, dos) {
+    init {
+        this.numeroUno; numeroUno;
+        this.numeroDos; numeroDos;
+    }
+    constructor(
+        uno: Int?,
+        dos: Int
+    ) : this (
+        if (uno == null) 0 else uno,
+        dos
+    ) {
+        numeroUno
+    }
+
+    constructor(
+        uno: Int,
+        dos: Int?,
+    ) : this(
+        uno,
+        if (dos == null ) 0 else uno
+    )
+}
+
+//Funciones
+fun imprimirNombre(nombre: String): Unit {
+    println("Nombre : ${nombre}")
+}
+
+fun calcularSueldo(
+    sueldo: Double,
+    tasa: Double = 12.00,
+    bonoEspecial: Double? = null,
+): Double {
+    if(bonoEspecial == null){
+        return sueldo * (100/tasa)
+    } else {
+        bonoEspecial.dec()
+        return sueldo * (100/tasa) + bonoEspecial
+    }
+}
+
+
+
