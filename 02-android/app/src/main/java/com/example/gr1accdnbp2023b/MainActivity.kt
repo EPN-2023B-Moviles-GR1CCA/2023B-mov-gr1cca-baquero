e909736cd9ae3765f934396830f20a1701f2f891
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2024-01-04 00:24:36 -0500

    hasta callbacks

--- 02-android/app/src/main/java/com/example/gr1accdnbp2023b/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/gr1accdnbp2023b/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.gr1accdnbp2023b
 
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.service.voice.VoiceInteractionSession.ActivityId
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +27,56 @@ class MainActivity : AppCompatActivity() {
             irActividad(BListView:: class.java)
         }
     }
+
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                if (result.data != null) {
+                    // Lógica Negocio
+                    val data = result.data
+                    mostrarSnackbar(
+                        "${data?.getStringExtra("nombreModificado")}"
+                    )
+                }
+            }
+        }
+
+    fun mostrarSnackbar(texto: String) {
+        Snackbar
+            .make(
+                findViewById(R.id.id_layout_main), //View
+                texto, //texto
+                Snackbar.LENGTH_LONG //tiempo
+            )
+            .show()
+    }
+
+    val callbackIntentImplicitoTelefono =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            result ->
+
+            if(result.resultCode === Activity.RESULT_OK) {
+                if (result.data != null) {
+                    if (result.data!!.data != null) {
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri, null, null, null, null, null)
+                        cursor?.moveToFirst()
+                        val indiceTelefonos = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefonos!!)
+                        cursor?.close()
+                        mostrarSnackbar("Teléfono ${telefono}")
+                    }
+                }
+            }
+        }
 
     fun irActividad(
         clase: Class<*>
