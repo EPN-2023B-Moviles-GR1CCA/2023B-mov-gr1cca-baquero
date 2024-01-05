99c035eededae3439082ce60ab82c3dcbdaace7a
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2024-01-04 19:21:34 -0500

    hasta clase 10

--- 02-android/app/src/main/java/com/example/gr1accdnbp2023b/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/gr1accdnbp2023b/MainActivity.kt
@@ -15,8 +15,11 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        //Base de datos sqlite
+        EBaseDeDatos.tablaEntrenador = ESqliteHelperEntrenador(this)
+
         val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
         botonCicloVida
             .setOnClickListener {
